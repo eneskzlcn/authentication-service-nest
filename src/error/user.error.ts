464652f5d31d3050edf05b635715d6b2dfd8b118
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-10-07 02:35:16 +0300

    unnecessary error types removed

--- src/error/user.error.ts
+++
@@ -1,6 +0,0 @@
-const UserWithUsernameAlreadyExistsError: Error = Error(
-  'User with given username already exists.',
-);
-const UserWithEmailAlreadyExistsError: Error = Error(
-  'User with given email already exists.',
-);
