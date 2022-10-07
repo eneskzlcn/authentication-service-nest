b9d4dba4ad9f7dbfcd58a9e94d13bc525de7370d
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-10-07 03:13:25 +0300

    user configured with logger and signup exception filter.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,27 +1,23 @@
-import {
-  Body,
-  Controller,
-  HttpException,
-  HttpStatus,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Logger, Post, UseFilters } from '@nestjs/common';
 import { UserService } from './user.service';
-import { User } from '../entity/user.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { isValidSignUpRequest, SignUpRequest } from './user';
+import { SignUpExceptionFilter } from './exception/user.filter';
+
+import { validateSignupRequest, SignUpRequest, SignUpResponse } from './user';
 
 @Controller('')
 export class UserController {
+  private readonly logger = new Logger(UserController.name);
+
   constructor(private readonly loginService: UserService) {}
+
   @Post('/signup')
-  signUp(@Body() signUpRequest: SignUpRequest) {
-    if (!isValidSignUpRequest(signUpRequest)) {
-      return new HttpException('', HttpStatus.BAD_REQUEST);
-    }
-    this.loginService.signUp(signUpRequest);
-    return '';
+  @UseFilters(SignUpExceptionFilter)
+  async signUp(@Body() signUpRequest: SignUpRequest): Promise<SignUpResponse> {
+    this.logger.debug(`Signup request arrived: ${signUpRequest}`);
+    validateSignupRequest(signUpRequest);
+    return this.loginService.signUp(signUpRequest);
   }
+
   @Post('/signin')
   signIn(@Body() signInRequest: string): string {
     return signInRequest;
