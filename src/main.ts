8e3ae6ab2a3c47013f56bf9ad9305fab2551efe3
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-10-08 22:21:55 +0300

    tslogger added to project

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@modules/app.module';
 import { ConfigService } from '@nestjs/config';
+import { logger } from '@logger/tslog.logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: false,
+  });
   const configService: ConfigService = app.get<ConfigService>(ConfigService);
   await app.listen(configService.get('SERVER_PORT'));
+  logger.info(
+    `Application started to listen at ${configService.get('SERVER_PORT')}`,
+  );
 }
 bootstrap();
