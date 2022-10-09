66c4ce63e7206e253cfd7052a791611ac7dc8eee
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-10-09 17:15:49 +0300

    main file configured to make a start log on start then log error catches.

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,16 @@ import { ConfigService } from '@nestjs/config';
 import { logger } from '@logger/tslog.logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-    logger: false,
-  });
-  const configService: ConfigService = app.get<ConfigService>(ConfigService);
-  await app.listen(configService.get('SERVER_PORT'));
-  logger.info(
-    `Application started to listen at ${configService.get('SERVER_PORT')}`,
-  );
+    const app = await NestFactory.create(AppModule, {
+        cors: true,
+        logger: false,
+    });
+    const configService: ConfigService = app.get<ConfigService>(ConfigService);
+    await app.listen(configService.get('SERVER_PORT'));
 }
-bootstrap();
+
+bootstrap()
+    .then(() => {
+        logger.info(`Application started to listening`);
+    })
+    .catch((error) => logger.error(error.message));
