e75ea0b96a819772f563af59d17788eba003bb1f
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-10-09 17:19:20 +0300

    new route for post new user.

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,39 +1,36 @@
 import {
-  Body,
-  Controller,
-  Logger,
-  Post,
-  UseFilters,
-  Res,
-  HttpStatus,
+    Body,
+    Controller,
+    HttpStatus,
+    Logger,
+    Post,
+    Res,
+    UseFilters,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { SignUpUserRelatedExceptionFilter } from './user.filter';
+import { BadUserRequestExceptionFilter } from '@filters/bad.request.exception.filter';
 import { DatabaseExceptionFilter } from '@filters/database.exception.filter';
 
-import { validateSignupRequest, SignUpRequest } from './user';
+import { CreateUserRequest, validateCreateUserRequest } from './user';
 import { Response } from 'express';
 
-@Controller('')
+@Controller('user')
 export class UserController {
-  private readonly logger = new Logger(UserController.name);
+    private readonly logger = new Logger(UserController.name);
 
-  constructor(private readonly loginService: UserService) {}
+    constructor(private readonly loginService: UserService) {}
 
-  @Post('/signup')
-  @UseFilters(SignUpUserRelatedExceptionFilter, DatabaseExceptionFilter)
-  async signUp(
-    @Body() signUpRequest: SignUpRequest,
-    @Res() response: Response,
-  ) {
-    this.logger.debug(`Signup request arrived: ${signUpRequest}`);
-    validateSignupRequest(signUpRequest);
-    const signUpResponseData = await this.loginService.signUp(signUpRequest);
-    return response.status(HttpStatus.OK).send(signUpResponseData);
-  }
-
-  @Post('/signin')
-  signIn(@Body() signInRequest: string): string {
-    return signInRequest;
-  }
+    @Post('')
+    @UseFilters(BadUserRequestExceptionFilter, DatabaseExceptionFilter)
+    async signUp(
+        @Body() createUserRequest: CreateUserRequest,
+        @Res() response: Response
+    ) {
+        this.logger.debug(`Signup request arrived: ${createUserRequest}`);
+        validateCreateUserRequest(createUserRequest);
+        const signUpResponseData = await this.loginService.create(
+            createUserRequest
+        );
+        return response.status(HttpStatus.OK).send(signUpResponseData);
+    }
 }
